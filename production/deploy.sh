55d70928c51720d9712d7c2b79f597fbd1fd6f9e
Yulia51188 <sun51188@gmail.com>
2022-05-19 12:16:25 +0300

    Update comments in deploy script

--- production/deploy.sh
+++ production/deploy.sh
@@ -8,11 +8,9 @@ docker cp burger_django:/backend/staticfiles/. ../static/
 docker cp burger_parcel:/frontend/bundles/. ../static/
 #Release
 docker exec -it burger_django python manage.py migrate --noinput
-# docker compose up -d
 systemctl restart docker-burger.service
-# docker compose up -d
 systemctl reload nginx.service
-#Logging
+#Logging to Rollbar
 REVISION=$(git rev-parse --short HEAD)
 ROLLBAR_TOKEN=$(cat .env | grep ROLLBAR_TOKEN | cut -d "=" -f 2)
 curl -H "Accept: application/json" -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production", "revision": "'"$REVISION"'", "status": "succeeded"}'
