4cee012e7f6b33272cb8e08b38cddce5e4c1dafb
Yuliya <sun51188@gmail.com>
2022-05-19 09:14:30 +0000

    Create deploy script

---
+++ production/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+#Build
+cd /opt/star-burger-d/
+git pull
+cd production
+docker compose build
+docker cp burger_django:/backend/staticfiles/. ../static/
+docker cp burger_parcel:/frontend/bundles/. ../static/
+#Release
+docker exec -it burger_django python manage.py migrate --noinput
+# docker compose up -d
+systemctl restart docker-burger.service
+# docker compose up -d
+systemctl reload nginx.service
+#Logging
+REVISION=$(git rev-parse --short HEAD)
+ROLLBAR_TOKEN=$(cat .env | grep ROLLBAR_TOKEN | cut -d "=" -f 2)
+curl -H "Accept: application/json" -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production", "revision": "'"$REVISION"'", "status": "succeeded"}'
+echo "Deploy $REVISION is finished successfully"
